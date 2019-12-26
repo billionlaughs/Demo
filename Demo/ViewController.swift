19af94e7275b41dc9f906a45814793a424ac0ff3
mukilarasan.r <mukilarasan.r@zohocorp.com>
2019-12-26 12:28:29 +0530

    Adding demo project's files.

---
+++ Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Mukilarasan R on 09/12/19.
+//  Copyright © 2019 Mukilarasan R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
